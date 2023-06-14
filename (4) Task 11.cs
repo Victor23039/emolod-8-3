d5ded5f63cade0990eeabfc186f308791cac9728
Victor23039 <134534515+Victor23039@users.noreply.github.com>
2023-06-14 09:16:31 +0300

    Add files via upload

---
+++ (4) Task 11.cs
@@ -0,0 +1,100 @@
+﻿using ConsoleApp3;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleApp3
+{
+    public class Car
+    {
+        private List<Unit> units;
+        Engire engire = null;
+        private List<Engire> goods;
+        public void addEngire (Engire engire)
+        {
+            if (engire != null)
+            {
+                if (this.engire == null)
+                {
+                    this.engire = engire;
+                }
+                else
+                {
+                    goods.Add (engire);
+                    this.engire = engire;
+                }
+            }
+        }
+    }
+        class Sedan : Car
+        {
+        public double brand = null
+        public double model = null
+        public int graduationYear = 0
+        public Sedan (double brand, double model, int GraduationYear)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.graduationYear = GraduationYear;
+        }
+        }
+        class Van : Car
+        {
+        {
+        public double brand = null
+        public double model = null
+        public int graduationYear = 0
+        public Van(double brand, double model, int GraduationYear)
+        {
+            this.brand = brand;
+            this.model = model;
+            this.graduationYear = GraduationYear;
+        }
+        }
+}
+        class Pickup : Car
+        {
+        public double brand = null
+        public double model = null
+        public int graduationYear = 0
+        public Pickup(double brand, double model, int GraduationYear)
+        {
+        this.brand = brand;
+        this.model = model;
+        this.graduationYear = GraduationYear;
+        }
+        }
+class Engire
+{
+    private int power = 0;
+    private int volume = 0;
+}
+public Engire(int power, int volume)
+{
+    this.power = power;
+    this.volume = volume;
+}   
+
+    internal class Program
+    {
+        static void Main(string[] args)
+    {
+        Car car = new car();
+        List<Sedan> list = new List<Sedan>();
+        Sedan roach = new Sedan(Toyota, bZ3, 2017)
+            list.Add(roach);
+        List<Van> list = new List<Van>();
+        Van roach = new Van(Ford, Transit, 2016)
+            list.Add(roach);
+        List<Pickup> list = new List<Pickup>();
+        Pickup roach = new Pickup(Nissan, Navara, 2022)
+            list.Add(roach);
+        Random rand = new Random ();
+        List<Engire> list = new List<Engire> ();
+        for (int i = 0; i<10; i++)
+        {
+            Engire roach = new Engire(rand.Next(12, 45), rand.Next(1, 13));
+            list.Add(roach);
+        }
+    }
+            
+    }
+}
---
+++ (4) Task 2.cs
@@ -0,0 +1,60 @@
+﻿int SS = 0;
+int PS = 0;
+int ST = 0;
+int SC = 0;
+int PC = 0;
+int PR = 0;
+int SR = 0;
+class Figure
+{
+    class Rectangle : Figure
+    {
+
+    }
+    class Square : Figure
+    {
+    
+    }
+    class Trapezium : Figure
+    {
+    
+    }
+    class Circle : Figure
+    {
+     
+    }
+}
+Console.WriteLine("=========================================================");
+Console.WriteLine("Введіть радіус (см): ");
+double rnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть висоту (см): ");
+double vnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть ширину (см): ");
+double snumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введіть довжину (см): ");
+double fnumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ведіть фігуру - прямокутник, квадрат, трапеція, коло: ");
+double figure = Console.ReadLine();
+switch (figure);
+case 'прямокутник':
+    Console.WriteLine("Площа");
+    SR = fnumber * snumber;
+    Console.WriteLine("Периметр");
+    PR = (fnumber + snumber) * 2;
+    break;
+case 'квадрат':
+    Console.WriteLine("Площа");
+    SS = fnumber ^ 2;
+    Console.WriteLine("Периметр");
+    PS = fnumber * 4;
+    break;
+case 'трапеція':
+    Console.WriteLine("Площа");
+    ST = (fnumber + snumber) / 2 * vnumber;
+    break;
+case 'коло':
+    Console.WriteLine("Площа");
+    SC = 3,14 * rnumber^2;
+    Console.WriteLine("Периметр");
+    PC = 2 * 3,14 * rnumber;
+    break;
